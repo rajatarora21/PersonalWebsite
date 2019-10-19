f32ccbf96b63b369078fcce4be6f27133283339a
rajatarora21 <arorarajat9999@gmail.com>
2019-10-18 21:30:17 -0400

    changed static files location

--- PersonalWebsite/PersonalWebsite/settings.py
+++ PersonalWebsite/PersonalWebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ywun$+f!9wh5$p3j3--q^60%a=fs&gujz$mqx!t-)@(scta&8v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
