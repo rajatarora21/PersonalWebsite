3d22c57248012fa4e87208ac18f2ca2fd8c90f60
rajatarora21 <arorarajat9999@gmail.com>
2019-11-10 01:18:48 -0500

    Google analytics
    Added google analytics to index.html to track users visiting website

--- PersonalWebsite/assets/js/main.js
+++ PersonalWebsite/assets/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Rajat Arora.", "Developer.", "Travel enthusiast.", "Pizza lover"],
       typeSpeed: 100,
       loop: true,
     });
